7b5ecbf4f82dde638b3735cb98836923cb2691f8
Malak2022 <100687093+Malak2022@users.noreply.github.com>
2023-12-10 13:20:34 +0100

    Prosit_3

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -3,21 +3,16 @@ public class ZooManagement {
     public static void main(String[] args) {
         Animal lion = new Animal();
         lion.name = "lion";
-        lion.age = 6;
+        lion.age = 5;
         lion.family = "chats";
-        lion.isMammal = false;
+        lion.isMammal = true;
 
-        Zoo myZoo = new Zoo();
-        myZoo.name = "Zoo";
-        myZoo.city = "Tunis";
-        myZoo.nbrCages = 25;
-        myZoo.animals = new Animal[25];
+        Zoo myZoo = new Zoo("Zoo", "Tunis");
+        Zoo notMyZoo = new Zoo("ParkTun", "Ben Arous");
 
 
-        Animal dog = new Animal("chien", "zara", 5, true);
+        Animal dog = new Animal("chien", "Rex", 3, true);
 
-        System.out.println(myZoo);
-        System.out.println(myZoo.toString());
 
         System.out.println(myZoo.addAnimal(lion));
         System.out.println(myZoo.addAnimal(dog));
@@ -25,13 +20,31 @@ public class ZooManagement {
         myZoo.displayAnimals();
 
         System.out.println(myZoo.searchAnimal(dog));
-        Animal dog1 = new Animal("chien", "rex", 3, true);
-        System.out.println(myZoo.searchAnimal(dog));
+        Animal dog1 = new Animal("chien", "Zara", 2, true);
+        System.out.println(myZoo.searchAnimal(dog1));
 
-        System.out.println(myZoo.removeAnimal(dog));
+        //   System.out.println(myZoo.removeAnimal(dog));
         myZoo.displayAnimals();
+
+
+        System.out.println(myZoo);
+
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(dog);
+        myZoo.addAnimal(dog1);
+        myZoo.displayAnimals();
+        System.out.println("a" + myZoo.removeAnimal(lion));
+        myZoo.displayAnimals();
+        System.out.println("a" + myZoo.removeAnimal(dog1));
+        myZoo.displayAnimals();
+        System.out.println("a" + myZoo.removeAnimal(dog));
+        myZoo.displayAnimals();
+
+//        System.out.println(Zoo.comparerZoo(myZoo, notMyZoo));
+//        System.out.println(myZoo.isZooFull());
 
     }
 
 }
+
 
